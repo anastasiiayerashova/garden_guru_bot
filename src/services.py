3d119c98385f2eee05394f6cc10afb68bc9be009
Anastasia Yerashova <yerashova.a@gmail.com>
2026-04-15 00:30:07 +0200

    add logs to services

--- src/services.py
+++ src/services.py
@@ -1,4 +1,5 @@
 import logging
+import time
 from init import agent
 
 
@@ -7,11 +8,22 @@ logger = logging.getLogger(__name__)
 
 
 async def get_agent_response(user_text: str, chat_id: int, session_id: int = 0) -> str:
-    config = {'configurable': {'thread_id': f"{str(chat_id)}_{session_id}"}}
+    thread_id = f'{str(chat_id)}_{session_id}'
+    config = {'configurable': {'thread_id': thread_id}}
+    
+    logger.info(f'💬 Отримано запит від користувача: "{user_text}" (Chat ID: {chat_id}, Thread ID: {thread_id})')
 
+    start_time = time.perf_counter()
+    
     try:
         result_state = await agent.ainvoke({'messages': [('user', user_text)]}, config=config)
-        return result_state['messages'][-1].content
+        response_content = result_state['messages'][-1].content
+
+        end_time = time.perf_counter()
+        duration = end_time - start_time
+
+        logger.info(f'✅ [Agent Response] Успішно за {duration:.2f}с')
+        return response_content
 
     except Exception as e:
         logger.error(f'LangGraph Error: {e}', exc_info=True)
